09de87212e0fdc6f377073ef0ae1f379f946e69c
SIJUZ <sijuz@yandex.ru>
2020-07-08 04:46:03 +0300

    1
    1

---
+++ precache-manifest.a91e53bd586e7be718aa6579cc9a3b39.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54e6828ca0f0b555a6421a123d4311e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9d68c91656fb6b577d9",
+    "url": "/static/css/2.8c9b10a9.chunk.css"
+  },
+  {
+    "revision": "febcddf323c9298fdf0a",
+    "url": "/static/css/main.fe381712.chunk.css"
+  },
+  {
+    "revision": "c9d68c91656fb6b577d9",
+    "url": "/static/js/2.1d90e5f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1d90e5f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "febcddf323c9298fdf0a",
+    "url": "/static/js/main.0cc4dda6.chunk.js"
+  },
+  {
+    "revision": "ec57ff62c2ffacd8ea15",
+    "url": "/static/js/runtime-main.70c60177.js"
+  },
+  {
+    "revision": "1eddcd7e65fbaae34961c3525cfbb189",
+    "url": "/static/media/fon.1eddcd7e.svg"
+  },
+  {
+    "revision": "1446db78832edd707e3322ff35b74f7c",
+    "url": "/static/media/logo.1446db78.svg"
+  },
+  {
+    "revision": "9d8d3085cfcae14c5326cc100c6de93f",
+    "url": "/static/media/logo_d.9d8d3085.svg"
+  }
+]);
