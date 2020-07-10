7197d5dd547c6f4ff0efb1b9db5fd6c76ec3bb60
SIJUZ <sijuz@yandex.ru>
2020-07-10 04:39:51 +0300

    1
    1

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c527c48273e46e1b84f5bb810402fd66.js"
+  "/precache-manifest.3d276282ada8e04f7a8122c72a7f7acc.js"
 );
 
 self.addEventListener('message', (event) => {
