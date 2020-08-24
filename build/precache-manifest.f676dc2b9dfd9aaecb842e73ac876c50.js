f6d4d4a62433a158eb9f5e4ba56d1e755838221a
LaCocaina <57399815+LaCocaina@users.noreply.github.com>
2020-08-24 15:35:58 +0300

    build
    bibip

---
+++ build/precache-manifest.f676dc2b9dfd9aaecb842e73ac876c50.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07f81f20fd45ad01f71387a8a74520fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "912679d26c9a65204c36",
+    "url": "/static/css/2.8c9b10a9.chunk.css"
+  },
+  {
+    "revision": "6d1a8de96a537134af26",
+    "url": "/static/css/main.b2ddcc1d.chunk.css"
+  },
+  {
+    "revision": "912679d26c9a65204c36",
+    "url": "/static/js/2.d0612977.chunk.js"
+  },
+  {
+    "revision": "590b288afc476ced67e9258cf268eacb",
+    "url": "/static/js/2.d0612977.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d1a8de96a537134af26",
+    "url": "/static/js/main.3ae16dd3.chunk.js"
+  },
+  {
+    "revision": "623fb580824fbe639ae3",
+    "url": "/static/js/runtime-main.cd548ab4.js"
+  },
+  {
+    "revision": "1eddcd7e65fbaae34961c3525cfbb189",
+    "url": "/static/media/fon.1eddcd7e.svg"
+  },
+  {
+    "revision": "8508205779f8eb209ee22a6fbd0ab470",
+    "url": "/static/media/fon_d.85082057.svg"
+  },
+  {
+    "revision": "1446db78832edd707e3322ff35b74f7c",
+    "url": "/static/media/logo.1446db78.svg"
+  },
+  {
+    "revision": "9d8d3085cfcae14c5326cc100c6de93f",
+    "url": "/static/media/logo_d.9d8d3085.svg"
+  },
+  {
+    "revision": "4b483f2b2377e3c88eec888752786fa9",
+    "url": "/static/media/yt.4b483f2b.svg"
+  }
+]);
