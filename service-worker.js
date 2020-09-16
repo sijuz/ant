334259a96a4d22816a6d7a455f6626b4f20ffa52
LaCocaina <57399815+LaCocaina@users.noreply.github.com>
2020-09-16 11:24:59 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c7e3af3e28163d3d30de8a6174eb40b.js"
+  "/precache-manifest.254b60cafda54a2d2365dc4abc7b782c.js"
 );
 
 self.addEventListener('message', (event) => {
